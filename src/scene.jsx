8d93a55ce57b8b7dc2a469161feda8b7d7c6314c
Bjorn Stromberg <bjorn@bjornstar.com>
2021-05-28 09:52:33 +0900

    Add onClick effect and move in a circle

--- src/scene.jsx
+++ src/scene.jsx
@@ -1,31 +1,39 @@
 import { useFrame } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 
-const targetFramerate = 1 / 60;
+const framerate = 60;
 
 export default () => {
   const ref = useRef();
 
+  const [active, setActive] = useState(false);
+  const [frame, setFrame] = useState(0);
   const [hovered, setHover] = useState(false);
 
   useFrame(({ raycaster }, delta) => {
-    const { current } = ref;
+    const { current, current: { position, rotation } } = ref;
 
     setHover(!!raycaster.intersectObject(current).length);
 
-    const framerateMultiplier = delta / targetFramerate;
-
-    const { rotation } = current;
+    const framerateMultiplier = delta * framerate;
 
     rotation.x += 0.01 * framerateMultiplier;
     rotation.y += 0.005 * framerateMultiplier;
-  })
+
+    setFrame(frame + delta);
+
+    position.x = Math.sin(frame);
+    position.y = Math.cos(frame);
+  });
+
+  const onClick = () => setActive(!active);
 
   const args = [1,1,1];
   const color = hovered ? 'purple' : 'orange';
+  const scale = active ? 1.5 : 1;
 
   return (
-    <mesh {...{ ref }}>
+    <mesh {...{ onClick, ref, scale }}>
       <boxGeometry {...{ args }} />
       <meshStandardMaterial {...{ color }} />
     </mesh>
